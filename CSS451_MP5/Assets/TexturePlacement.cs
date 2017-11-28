ea480f5063f331ab47433c838681c4ea7ddaed1b
Jayse Farrell <jafarrellpersonal@gmail.com>
2017-11-28 15:50:34 -0800

    rot
    rotation of texture

--- CSS451_MP5/Assets/TexturePlacement.cs
+++ CSS451_MP5/Assets/TexturePlacement.cs
@@ -6,6 +6,9 @@ public class TexturePlacement : MonoBehaviour {
 
 	public Vector2 Offset = Vector2.zero;
 	public Vector2 Scale = Vector2.one;
+	public float Rotation = 0;
+
+
 	Vector2[] mInitUV = null; // initial values
 
 	public void SaveInitUV(Vector2[] uv)
@@ -20,11 +23,16 @@ public class TexturePlacement : MonoBehaviour {
 		Mesh theMesh = GetComponent<MeshFilter>().mesh;
 		Vector2[] uv = theMesh.uv;
 
+		Matrix3x3 rotMat = Matrix3x3Helpers.CreateRotation (Rotation);
+
 		for (int i = 0; i < uv.Length; i++)
 		{
 			uv[i].x = mInitUV[i].x * Scale.x;
 			uv[i].y = mInitUV[i].y * Scale.y;
 			uv[i] = Offset + uv[i];
+
+
+			uv[i] = Matrix3x3.MultiplyVector2 (rotMat, uv[i]);
 		}
 		theMesh.uv = uv;
 	}
